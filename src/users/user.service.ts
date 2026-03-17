fb915ea0ca099540b42ac83d3503937a9e64eaa3
Victor Ozoterio <victorozoterio@gmail.com>
2026-03-17 00:16:30 -0300

    feat: cria serviço de usuários

---
+++ src/users/user.service.ts
@@ -0,0 +1,120 @@
+import { UserEntity } from "./user.entity";
+import { UserRepository } from "./user.repository";
+
+export type CreateUserInput = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type CreateUserOutput = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type FindAllUsersOutput = {
+  users: {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }[];
+};
+
+export type GetUserOutput = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type UpdateUserInput = {
+  id: string;
+  name: string;
+};
+
+export class UserService {
+  constructor(private readonly repository: UserRepository) {}
+
+  async create(input: CreateUserInput): Promise<CreateUserOutput> {
+    const user = UserEntity.create(
+      input.name,
+      input.email,
+      input.password
+    );
+
+    await this.repository.create(user);
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+
+  async findAll(): Promise<FindAllUsersOutput> {
+    const users = await this.repository.findAll();
+
+    return {
+      users: users.map((user) => ({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      })),
+    };
+  }
+
+  async findById(id: string): Promise<GetUserOutput> {
+    const user = await this.repository.findById(id);
+
+    if (!user) {
+      throw new Error(`User ${id} not found`);
+    }
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
+
+  async update(input: UpdateUserInput): Promise<UpdateUserInput> {
+    const user = await this.repository.findById(input.id);
+
+    if (!user) {
+      throw new Error(`User ${input.id} not found`);
+    }
+
+    if (input.name) {
+      user.updateName(input.name);
+    }
+
+    await this.repository.update(user);
+    
+    return {
+      id: user.id,
+      name: user.name,
+    };
+  }
+
+  async delete(id: string): Promise<void> {
+    const user = await this.repository.findById(id);
+
+    if (!user) {
+      throw new Error(`User ${id} not found`);
+    }
+
+    return await this.repository.delete(id);
+  }
+}
