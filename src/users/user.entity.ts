dd80583a913f51e4bd5ccf9b93cccf71f7b79672
Victor Ozoterio <victorozoterio@gmail.com>
2026-03-18 00:37:35 -0300

    feat: implementa repositório de usuários com prisma

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,52 +1,56 @@
 export type UserProps = {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
 };
 
 export class UserEntity {
-    private constructor(readonly props: UserProps) {}
+  private constructor(readonly props: UserProps) {}
 
-    public static create(name: string, email: string, password: string) {
-        return new UserEntity({
-            id: crypto.randomUUID(),
-            name,
-            email,
-            password,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-        });
-    }
+  public static create(name: string, email: string, password: string) {
+    return new UserEntity({
+      id: crypto.randomUUID(),
+      name,
+      email,
+      password,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
 
-    get id() {
-        return this.props.id;
-    }
+  public static fromPersistence(props: UserProps) {
+    return new UserEntity(props);
+  }
 
-    get name() {
-        return this.props.name;
-    }
+  get id() {
+    return this.props.id;
+  }
 
-    get email() {
-        return this.props.email;
-    }
+  get name() {
+    return this.props.name;
+  }
 
-    get password() {
-        return this.props.password;
-    }
+  get email() {
+    return this.props.email;
+  }
 
-    get createdAt() {
-        return this.props.createdAt;
-    }
+  get password() {
+    return this.props.password;
+  }
 
-    get updatedAt() {
-        return this.props.updatedAt;
-    }
+  get createdAt() {
+    return this.props.createdAt;
+  }
 
-    public updateName(name: string) {
-        this.props.name = name;
-        this.props.updatedAt = new Date();
-    }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  public updateName(name: string) {
+    this.props.name = name;
+    this.props.updatedAt = new Date();
+  }
 }
