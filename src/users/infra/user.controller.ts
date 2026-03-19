c62d0d9fc8d197aa311e169783d04eadd196ea7f
Victor Ozoterio <victorozoterio@gmail.com>
2026-03-18 22:56:18 -0300

    feat: cria controlador de usuários

--- src/users/infra/user.controller.ts
+++ src/users/infra/user.controller.ts
@@ -1,11 +1,73 @@
 import { Request, Response } from "express";
+import { prisma } from "../../infra/database/prisma.client";
+import { UserService } from "../application/user.service";
+import { UserRepositoryPrisma } from "./user.repository.prisma";
 
 export class UserController {
+  private readonly service: UserService;
+
+  private constructor() {
+    const repository = new UserRepositoryPrisma(prisma);
+    this.service = new UserService(repository);
+  }
+
   public static build() {
     return new UserController();
   }
-  
-  public async get(request: Request, response: Response) {
-    response.status(200).json({ message: "Hello World" }).send();
+
+  public async create(request: Request, response: Response) {
+    const { name, email, password } = request.body;
+
+    const output = await this.service.create({
+      name,
+      email,
+      password,
+    });
+
+    response.status(201).json(output).send();
+  }
+
+  public async findAll(request: Request, response: Response) {
+    const output = await this.service.findAll();
+
+    response.status(200).json(output).send();
+  }
+
+  public async findById(request: Request<{ id: string }>, response: Response) {
+    const { id } = request.params;
+    if (!id) {
+      return response.status(400).json({ message: "Id is required" });
+    }
+
+    const output = await this.service.findById(id);
+
+    response.status(200).json(output).send();
+  }
+
+  public async update(request: Request<{ id: string }>, response: Response) {
+    const { id } = request.params;
+    if (!id) {
+      return response.status(400).json({ message: "Id is required" });
+    }
+    
+    const { name } = request.body;
+
+    const output = await this.service.update({
+      id,
+      name,
+    });
+
+    response.status(200).json(output).send();
+  }
+
+  public async delete(request: Request<{ id: string }>, response: Response) {
+    const { id } = request.params;
+    if (!id) {
+      return response.status(400).json({ message: "Id is required" });
+    }
+
+    await this.service.delete(id);
+
+    response.status(204).send();
   }
 }
--- src/users/infra/user.routes.ts
+++ src/users/infra/user.routes.ts
@@ -5,7 +5,11 @@ export const userRoutes = () => {
   const router = Router();
   const controller = UserController.build();
 
-  router.get("/", (req, res) => controller.get(req, res));
+  router.post("/", (req, res) => controller.create(req, res));
+  router.get("/", (req, res) => controller.findAll(req, res));
+  router.get("/:id", (req, res) => controller.findById(req, res));
+  router.put("/:id", (req, res) => controller.update(req, res));
+  router.delete("/:id", (req, res) => controller.delete(req, res));
 
   return router;
 };
